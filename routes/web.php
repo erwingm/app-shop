67306276caeeb33c0d6295a5451a2ce3028fec37
erwingm <erwin.guerra.mamani@gmail.com>
2018-02-01 19:29:27 -0500

    Proyecto Base}

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+Route::get('/','TestController@welcome');
+
+Route::get('/prueba', function(){
+
+	return 'Hola soy una ruta de prueba';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
