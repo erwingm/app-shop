7a81537a0798cea3b278a7e3d0bb3b70bb6516f0
erwingm <erwin.guerra.mamani@gmail.com>
2018-03-11 19:29:09 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                 <ul class="nav navbar-nav navbar-right">
                   @guest
                             <li><a href="{{ route('login') }}">Ingresar</a></li>
-                            <li><a href="{{ route('register') }}">Registro</a></li>
+                            <!-- <li><a href="{{ route('register') }}">Registro</a></li> -->
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
